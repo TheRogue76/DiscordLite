db0c1a005a6d95dcd9593f936b2aedbdf6f78ac1
Parsa Nasirimehr <p.nasirimehr@gmail.com>
2025-12-25 16:31:41 +0100

    Initial Commit

---
+++ DiscordLite/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiscordLite
+//
+//  Created by Parsa's Content Creation Corner on 2025-12-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiscordLite/DiscordLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscordLiteApp.swift
+//  DiscordLite
+//
+//  Created by Parsa's Content Creation Corner on 2025-12-25.
+//
+
+import SwiftUI
+
+@main
+struct DiscordLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
